1c55df4279951c5a424dd96368df8f3b807c7ed1
Advait-Sen <advaitsen78@gmail.com>
2024-11-28 23:23:17 +0100

    Making assignment operators different

--- src/parser/node/operator/OperatorType.java
+++ src/parser/node/operator/OperatorType.java
@@ -11,10 +11,18 @@ import static parser.node.operator.Operator.operatorType;
 public enum OperatorType {
     SUM("+", 10),
     DIFFERENCE("-", 10),
-    ASSIGN("=", 1, false),
+    ASSIGN("="),
 
     NEGATE("!", 15, false, 1),
     ;
+
+    // Constructor for assignment, since we treat that differently in tokeniser itself
+    OperatorType(String value){
+        this.value = value;
+        this.precedence = 0;
+        this.isLeftAssoc = false;
+        this.args = -1;
+    }
 
     OperatorType(String value, int precedence) {
         this.value = value;
@@ -47,6 +55,7 @@ public enum OperatorType {
         for (OperatorType op : OperatorType.values()) {
             operatorType.put(op.value, op);
             Keywords.operatorTokens.put(op.value, switch (op.args){
+                case -1-> TokenType.ASSIGNMENT_OPERATOR;
                 case 1-> TokenType.UNARY_OPERATOR;
                 case 2-> TokenType.BINARY_OPERATOR;
                 default -> throw new RuntimeException("How did we get here? Had illegal number of arguments for operator");
